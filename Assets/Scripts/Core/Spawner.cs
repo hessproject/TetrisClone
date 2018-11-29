0bb7abd393ac399bfc3228ae679e86bcfd9871fa
Nick Hess <hessproject@gmail.com>
2018-11-28 19:27:40 -0500

    Add GameController and Spawner class

---
+++ Assets/Scripts/Core/Spawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public Shape[] m_allShapes;
+
+    Shape GetRandomShape()
+    {
+        return m_allShapes[Random.Range(0, m_allShapes.Length)];
+    }
+
+    public Shape SpawnShape()
+    {
+        Shape shape = Instantiate(GetRandomShape(), transform.position, Quaternion.identity) as Shape;
+        if (shape)
+        {
+            return shape;
+        }
+        else
+        {
+            Debug.LogWarning("Warning: Unable to spawn shape");
+            return null;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Managers/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    Board m_gameBoard;
+    Spawner m_spawner;
+
+	// Use this for initialization
+	void Start () {
+        m_gameBoard = GameObject.FindWithTag("Board").GetComponent<Board>();
+        m_spawner = GameObject.FindWithTag("Spawner").GetComponent<Spawner>();
+
+        if (!m_gameBoard)
+        {
+            Debug.LogWarning("Warning: No game board defined");
+        }
+
+        if (!m_spawner)
+        {
+            Debug.LogWarning("Warning: No spawner defined");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
